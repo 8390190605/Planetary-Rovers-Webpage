b3e86656feb2672de35697e6d6cb18c710ab2d6c
8390190605 <83525584+8390190605@users.noreply.github.com>
2022-06-20 12:49:26 +0530

    Add files via upload

---
+++ Planatory Rovers/index.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Planatory Rovers</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    <body background="https://i.pinimg.com/564x/c6/83/65/c68365e54b6d4af872cce9b758a4e41a.jpg" style="background-repeat: no-repeat;background-size: 100% 100%;" >
+<br>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light shadow-5-strong">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style="color:white;" ><h2>Planatory Rovers</h2></a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">Services</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">Contact us</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">Login</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">Upload</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="btn btn-outline-color-black" aria-current="page" style="color: white;" href="#">Project</a>
+        </li>
+
+         <form class="d-flex" role="search">
+           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="width: 27rem;">
+           <button class="btn btn-outline-color-black" style="color: white;" type="submit" >Search</button>
+         </form>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<br>
+<br>
+<!--projects-->
+<main>
+   <div class = "container my-4">
+     <div class="album py-5 bg-colour-black">
+       <div class="container">
+
+         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/NASA_Mars_Rover.jpg/800px-NASA_Mars_Rover.jpg" alt="Opportunity Rover">
+               <div class="card-body">
+                  <h3>Opportunity Rover</h3>
+                 <p class="card-text">Opportunity, also known as MER-B (Mars Exploration Rover–B) or MER-1, and nicknamed Oppy, is a robotic rover that was active on Mars from 2004 until mid-2018. Opportunity was operational on Mars for 5110 sols. Launched on July 7, 2003, as part of NASA's Mars Exploration Rover program, it landed in Meridiani Planum on January 25, 2004, three weeks after its twin Spirit (MER-A) touched down on the other side of the planet. </p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-color-black">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Lunokhod-2_model.jpg/800px-Lunokhod-2_model.jpg" alt="Lunokhod 2 Rover">
+               <div class="card-body">
+                  <h3>Lunokhod 2</h3>
+                 <p class="card-text">Lunokhod 2 (Russian: Луноход-2 ("Moonwalker 2"), also known as Аппарат 8ЕЛ № 204 ("Device 8EL No. 204")) was the second of two unmanned lunar rovers that landed on the Moon by the Soviet Union as part of the Lunokhod programme. The Luna 21 spacecraft landed on the Moon and deployed the second Soviet lunar rover, Lunokhod 2, in January 1973.</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5 ">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/NASA_Apollo_17_Lunar_Roving_Vehicle.jpg/800px-NASA_Apollo_17_Lunar_Roving_Vehicle.jpg" alt="Lunar Roving Vehicle">
+               <div class="card-body">
+                  <h3>Lunar Roving Vehicle</h3>
+                 <p class="card-text">The Lunar Roving Vehicle (LRV) is a battery-powered four-wheeled rover used on the Moon in the last three missions of the American Apollo program (15, 16, and 17) during 1971 and 1972. It is popularly called the Moon buggy, a play on the term dune buggy.</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Curiosity_Self-Portrait_at_%27Big_Sky%27_Drilling_Site.jpg/800px-Curiosity_Self-Portrait_at_%27Big_Sky%27_Drilling_Site.jpg" alt="Curiosity Rover">
+               <div class="card-body">
+                  <h3>Curiosity Rover</h3>
+                 <p class="card-text">Curiosity is a car-sized Mars rover designed to explore the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission.[2] Curiosity was launched from Cape Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6 August 2012, 05:17:57 UTC.[3][4][5] The Bradbury Landing site was less than 2.4 km (1.5 mi) from the center of the rover's touchdown target after a 560 million km (350 million mi) journey.</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Soviet_moonrover.JPG/800px-Soviet_moonrover.JPG" alt="Lunokhod 1 Rover">
+               <div class="card-body">
+                  <h3>Lunokhod 1 Rover</h3>
+                 <p class="card-text">Lunokhod 1 (Russian: Луноход-1 ("Moonwalker 1"), also known as Аппарат 8ЕЛ № 203 ("Device 8EL No. 203")) was the first of two robotic lunar rovers landed on the Moon by the Soviet Union as part of its Lunokhod program. The Luna 17 spacecraft carried Lunokhod 1 to the Moon in 1970. Lunokhod 1 was the first remote-controlled robot "rover" to freely move across the surface of an astronomical object beyond the Earth. It was also the first wheeled craft on another celestial body.[1] Lunokhod 0 (No.201), the previous and first attempt to do so, launched in February 1969 but failed to reach orbit. Although only designed for a lifetime of three lunar days (approximately three Earth months), Lunokhod 1 operated on the lunar surface for eleven lunar days (321 Earth days) and traversed a total distance of 10.54 km.</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/KSC-03PD-0786.jpg/1024px-KSC-03PD-0786.jpg" alt="Spirit Rover">
+               <div class="card-body">
+                  <h3>Spirit Rover</h3>
+                 <p class="card-text">Spirit, also known as MER-A (Mars Exploration Rover – A) or MER-2, is a Mars robotic rover, active from 2004 to 2010.[2] Spirit was operational on Mars for 2208 sols or 3.3 Martian years (2249 days; 6 years, 77 days). It was one of two rovers of NASA's Mars Exploration Rover Mission managed by the Jet Propulsion Laboratory (JPL). Spirit landed successfully within the impact crater Gusev on Mars at 04:35 Ground UTC on January 4, 2004, three weeks before its twin, Opportunity (MER-B), which landed on the other side of the planet. Its name was chosen through a NASA-sponsored student essay competition. The rover got stuck in a "sand trap" in late 2009 at an angle that hampered recharging of its batteries; its last communication with Earth was on March 22, 2010.</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Marspathrov.jpg/220px-Marspathrov.jpg" alt="Sojourner Rover">
+               <div class="card-body">
+                  <h3>Sojourner Rover</h3>
+                 <p class="card-text">Sojourner is a robotic Mars rover that landed in the Ares Vallis channel in the Chryse Planitia region of the Oxia Palus quadrangle on July 4, 1997. Sojourner was operational on Mars for 92 sols (95 Earth days). It was the first wheeled vehicle to rove on a planet other than Earth and formed part of the Mars Pathfinder mission. The rover was equipped with front and rear cameras, and hardware that was used to conduct several scientific experiments. It was designed for a mission lasting 7 sols, with a possible extension to 30 sols,[2] and was active for 83 sols (85 Earth days). </p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/ChangE-4%2C_Yutu-2_%28cropped%29.png/1024px-ChangE-4%2C_Yutu-2_%28cropped%29.png" alt="Yutu 2 Rover">
+               <div class="card-body">
+                  <h3>Yutu 2 Rover</h3>
+                 <p class="card-text">Yutu-2 is the robotic lunar rover component of CNSA's Chang'e 4 mission to the Moon, launched on 7 December 2018 18:23 UTC, it entered lunar orbit on 12 December 2018 before making the first soft landing on the far side of the Moon on 3 January 2019. Yutu-2 is currently operational as the longest-lived lunar rover[9] and the first lunar rover traversing the far side of the Moon.</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+
+           <div class="col">
+             <div class="card shadow-sm my-5">
+               <img src="https://static01.nyt.com/images/2020/03/03/science/26CHINAMOON1/26CHINAMOON1-mediumSquareAt3X.jpg" alt="Opportunity Rover">
+               <div class="card-body">
+                  <h3>Yutu Rover</h3>
+                 <p class="card-text">Yutu (Chinese: 玉兔; pinyin: Yùtù; lit. Jade Rabbit) was a robotic lunar rover that formed part of the Chinese Chang'e 3 mission to the Moon. It was launched at 17:30 UTC on 1 December 2013, and reached the Moon's surface on 14 December 2013.[7] The mission marks the first soft landing on the Moon since 1976 and the first rover to operate there since the Soviet Lunokhod 2 ceased operations on 11 May 1973.[</p>
+                 <div class="d-flex justify-content-between align-items-center">
+                   <div class="btn-group">
+                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </main>
+           </div>
+           </div>
+       </div>       
+   </div>
+  </body>
+</html>
---
+++ Planatory Rovers/write.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+	<div class="mb-5">
+	  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+	  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter your email adress">
+	</div>
+	<div class="mb-5">
+	  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+	  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter your password">
+  </body>
+</html>
